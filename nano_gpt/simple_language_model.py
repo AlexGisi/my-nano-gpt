Fix normalization

nano_gpt/simple_language_model.py
@@ -77,6 +77,7 @@ class FeedForward(nn.Module):
 class Head(nn.Module):
     def __init__(self, head_size):
         super().__init__()
+        self.head_size = head_size
         self.key = nn.Linear(embed_dim_n, head_size, bias=False)  # Linear projections.
         self.query = nn.Linear(embed_dim_n, head_size, bias=False)
         self.value = nn.Linear(embed_dim_n, head_size, bias=False)
@@ -87,7 +88,7 @@ class Head(nn.Module):
         B, T, C = x.shape
         k = self.key(x)
         q = self.query(x)
-        wei = q @ k.transpose(-2, -1) * C**-0.5
+        wei = q @ k.transpose(-2, -1) * self.head_size**-0.5
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # Decoder
         wei = F.softmax(wei, dim=-1)
         wei = self.dropout(wei)